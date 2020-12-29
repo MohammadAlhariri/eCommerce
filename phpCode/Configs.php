d8b8f0590c92af1de6b05d123d0c51f89c14d13a
zaidra <zaid.sarc@gmail.com>
2020-12-29 19:14:37 +0200

    new Config file

---
+++ phpCode/Configs.php
@@ -0,0 +1,14 @@
+<?php
+    echo "Configs File";
+    $Configs = (object) array(
+    'hostname' => 'localhost',
+    'USERNAME' => 'USERNAME',
+    'password' => 'PASSWORD',
+    'database' => 'DATABASE'
+    );
+
+
+    $CONNECTION = MYSQL_CONNECTION($Configs[hostname], $configs[username], $configs[password], $configs[database])
+    or die("Not Connected")
+
+?>
